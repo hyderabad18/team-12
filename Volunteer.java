ff6976b9f32be8701b31ea4f5c6a0aead3ef441f
unknown <navyakovuru@gmail.com>
2018-07-14 23:42:52 +0530

    volunteer java file

--- Volunteer.java
+++ Volunteer.java
@@ -4,28 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
 
 public class Volunteer extends AppCompatActivity {
-    private Button upl,lout;
+    private Button upld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer);
-        upl=(Button)findViewById(R.id.upload);
-        lout=(Button)findViewById(R.id.logout);
-        upl.setOnClickListener(new View.OnClickListener() {
+        upld=(Button)findViewById(R.id.upload);
+        upld.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent intent=new Intent(Volunteer.this,Upload.class);
                 startActivity(intent);
-            }
-        });
-        lout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Intent intent=new Intent(Volunteer.this,Upload.class);
-                //startActivity(intent);
             }
         });
     }
