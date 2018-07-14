73d38da6ec37318cc777f9047f6c892d0684c4c7
HarshRookie <harshrathi71@gmail.com>
2018-07-15 02:13:11 +0530

    Downloading audio file in phone

---
+++ MainAdmin.java
@@ -0,0 +1,106 @@
+package in.jpmc.student;
+
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainAdmin extends AppCompatActivity {
+
+    StorageReference mstorage, filepath, down;
+    private Button btm;
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_admin);
+        btm = (Button) findViewById(R.id.button2);
+        btm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainAdmin.this, "Downloading", Toast.LENGTH_SHORT).show();
+                mstorage = FirebaseStorage.getInstance().getReference();
+                //startdownload();
+                signInAnonymously();
+            }
+        });
+
+
+    }
+
+    private void signInAnonymously() {
+        mAuth.signInAnonymously().addOnSuccessListener(this, new  OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                // do your stuff
+                startdownload();
+            }
+        })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.e("As", "signInAnonymously:FAILURE", exception);
+                    }
+                });
+    }
+
+    private void startdownload() {
+        //StorageReference filepath = mstorage.child("audio").child("new_Audio.mp3");
+//download
+        down = mstorage.child("audio").child("new_audio.mp3");
+
+        File localFile = null;
+        try {
+            localFile = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "new_Audio.mp3");
+            localFile.createNewFile();
+            down.getFile(localFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        down.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getApplicationContext(), "Downloaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+        });
+
+    }
+
+}
+
+                // do your stuf
+
