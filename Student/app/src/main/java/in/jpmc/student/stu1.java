3f160d3ad89d2036d60ea20e93161cef589f46f7
Jyothsna98 <mullapudi.sister2@gmail.com>
2018-07-14 13:46:18 +0530

    master

---
+++ Student/app/src/main/java/in/jpmc/student/stu1.java
@@ -0,0 +1,13 @@
+package in.jpmc.student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class stu1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu1);
+    }
+}
