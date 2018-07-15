19122fc131712a71c6d2ffbb6c89942affb75bfd
pinnamaneniaishwarya <aishu.aishwarya5551@gmail.com>
2018-07-15 06:28:19 +0530

    speech

--- speechactivity.java
+++ speechactivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Button;
